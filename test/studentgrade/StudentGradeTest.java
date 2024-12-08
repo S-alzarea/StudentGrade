083400bf45fea03302506e4c22d782bed54a676f
solaimanAlshammari <Solaimankhsh@gmail.com>
2024-12-09 02:28:13 +0300

    Update StudentGradeTest.java
    add 2 test cases for C and D grades

--- test/studentgrade/StudentGradeTest.java
+++ test/studentgrade/StudentGradeTest.java
@@ -26,8 +26,22 @@ public class StudentGradeTest extends TestCase {
     }
     
     public void testGetGrade75() {
-        System.out.println("getGrade 85");
+        System.out.println("getGrade 75");
         int mark = 75;
+        String expResult = "C";
+        String result = StudentGrade.getGrade(mark);
+        assertEquals(expResult, result);
+    }
+      public void testGetGrade65() {
+        System.out.println("getGrade 65");
+        int mark = 65;
+        String expResult = "D";
+        String result = StudentGrade.getGrade(mark);
+        assertEquals(expResult, result);
+    }
+      public void testGetGrade55() {
+        System.out.println("getGrade 55");
+        int mark = 55;
         String expResult = "F";
         String result = StudentGrade.getGrade(mark);
         assertEquals(expResult, result);
